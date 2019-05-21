c0dc386984887f37df765dc1751bfd4cdb099c18
Denis Bystruev <dbystruev@me.com>
2019-05-21 22:20:54 +0300

    Visualised Plane Detection

--- Detecting Planes/ViewController.swift
+++ Detecting Planes/ViewController.swift
@@ -10,24 +10,21 @@ import UIKit
 import SceneKit
 import ARKit
 
-class ViewController: UIViewController, ARSCNViewDelegate {
+class ViewController: UIViewController {
 
     @IBOutlet var sceneView: ARSCNView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Add world origin and feature points
+        sceneView.debugOptions = [.showFeaturePoints, .showWorldOrigin]
+        
         // Set the view's delegate
         sceneView.delegate = self
         
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
-        
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -35,6 +32,9 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
+        
+        // Enable plane detection
+        configuration.planeDetection = [.horizontal]
 
         // Run the view's session
         sceneView.session.run(configuration)
@@ -46,30 +46,61 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Pause the view's session
         sceneView.session.pause()
     }
+}
 
-    // MARK: - ARSCNViewDelegate
+// MARK: - ARSCNViewDelegate
+extension ViewController: ARSCNViewDelegate {
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
+        
+        let floor = createFloor(planeAnchor: planeAnchor)
+        node.addChildNode(floor)
+        
+        let ship = createShip(planeAnchor: planeAnchor)
+        node.addChildNode(ship)
+    }
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
+        guard let planeNode = node.childNodes.first else { return }
+        guard let plane = planeNode.geometry as? SCNPlane else { return }
+        
+        plane.width = CGFloat(planeAnchor.extent.x)
+        plane.height = CGFloat(planeAnchor.extent.z)
+        
+        planeNode.position = SCNVector3(planeAnchor.center.x, 0, planeAnchor.center.z)
+    }
+    
+    /// Creates and returns horizontal plane
+    ///
+    /// - Parameter planeAnchor: ARPlaneAncchor for the plane
+    /// - Returns: SCNNode for created plane
+    func createFloor(planeAnchor: ARPlaneAnchor) -> SCNNode {
+        let extent = planeAnchor.extent
+        let width = CGFloat(extent.x)
+        let height = CGFloat(extent.z)
+        
+        let plane = SCNPlane(width: width, height: height)
+        plane.firstMaterial?.diffuse.contents = UIColor.blue
+        
+        let node = SCNNode(geometry: plane)
+        node.eulerAngles.x = -.pi / 2
+        node.opacity = 0.125
+        
         return node
     }
-*/
     
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
+    /// Create and return an SCNNode for ship
+    ///
+    /// - Parameter planeAnchor: plane anchor to place ship on
+    /// - Returns: SCNNode created
+    func createShip(planeAnchor: ARPlaneAnchor) -> SCNNode {
+        let node = SCNScene(named: "art.scnassets/ship.scn")!.rootNode.clone()
         
+        node.position = SCNVector3(planeAnchor.center.x, 0, planeAnchor.center.z)
+        node.scale = SCNVector3(0.1, 0.1, 0.1)
+        
+        return node
     }
     
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
-    }
 }
